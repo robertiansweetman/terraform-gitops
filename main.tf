46a876f24e5a06effe059d4b141aea83882020cc
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-05-29 21:34:14 +0100

    run init

---
+++ main.tf
@@ -0,0 +1,25 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+#   use_msi = true
+
+#   backend "azurerm" {
+#     storage_account_name = "abcd1234"
+#     container_name       = "tfstate"
+#     key                  = "prod.terraform.tfstate"
+#     subscription_id      = ${{ secrets.ARM_SUBSCRIPTION_ID }}
+#     tenant_id            = "00000000-0000-0000-0000-000000000000"
+  #}
+}
