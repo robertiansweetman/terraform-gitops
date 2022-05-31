f99528c66f441a5de5927d68e49cbd76bb918312
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-05-31 18:28:32 +0100

    add missing values

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ data "azurerm_resource_group" "existing" {
 
 resource "azurerm_storage_account" "bob" {
   name                     = "bob"
-  resource_group_name      = azurerm_resource_group.existing.name
-  location                 = azurerm_resource_group.existing.location
+  resource_group_name      = data.azurerm_resource_group.existing.name
+  location                 = data.azurerm_resource_group.existing.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
