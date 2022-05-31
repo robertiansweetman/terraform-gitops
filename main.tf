3b0680200445305069ffaaa94299cb3c07eb9bf8
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-05-31 18:17:08 +0100

    create a storage account

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-resource "random_pet" "rg-name" {
-  prefix    = var.resource_group_name_prefix
+data "azurerm_resource_group" "existing" {
+  name      = "Identity"
+  location  = "uksouth"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name      = random_pet.rg-name.id
-  location  = var.resource_group_location
+resource "azurerm_storage_account" "bob" {
+  name                     = "bob"
+  resource_group_name      = azurerm_resource_group.existing.name
+  location                 = azurerm_resource_group.existing.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
 }
+
+resource "azurerm_storage_container" "bob" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.bob.name
+  container_access_type = "private"
+}
+
