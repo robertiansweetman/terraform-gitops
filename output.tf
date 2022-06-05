503780736624d6ad0cdf9b8dd1edc9229133da81
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-06-05 11:46:01 +0100

    see output values

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "resource_group_name" {
     value = data.azurerm_resource_group.existing.name
 }
+
+output "azurerm_user_assigned_identity" {
+    value = data.azurerm_user_assigned_identity.existing
+}
